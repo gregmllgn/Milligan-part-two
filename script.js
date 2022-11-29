8033e317b9aabd492894d3cc81bc2a8c8d1799f8
Greg <gregmllgn@gmail.com>
2022-11-29 16:18:50 -0500

    'initial commit'

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ let userName = prompt("Welcome to GC mini golf! What is your name");
 let roundCount = prompt("Hi "+ userName +", Would you like to play 3 or 6 holes today?");
 let total = 0;
 
-
-
-
 for (let i = 1; i <= roundCount; i++ ) {
     totalPutts = Number(prompt("How many putts for hole " + i + "? (par is 3)"));
     total += totalPutts;
     console.log(total);   
 }
-  
 
-
-if (total ==9 && roundCount == 3) {
+  if (total ==9 && roundCount == 3) {
     console.log("Good game, " + userName + ". Your total par was: 0"); 
   
 } else if (total > 9 && roundCount == 3) {
